bf11370725febd0adc889c53fd05d166a08d7dcd
j-woodlee <jwoodlee@lion.lmu.edu>
2015-11-02 23:21:44 -0800

    added more comprehensive and useful methods to DequeNode

--- LinkedDeque/DequeNode.java
+++ LinkedDeque/DequeNode.java
@@ -4,6 +4,23 @@ public class DequeNode {
 
     public DequeNode(Object info) {
         this.info = info;
+        this.toTheLeft = null;
+        this.toTheRight = null;
     }
-    
+
+    public DequeNode getLeft() {
+        return this.toTheLeft;
+    }
+
+    public DequeNode getRight() {
+        return this.toTheRight;
+    }
+
+    public void setRight(DequeNode d) {
+        this.toTheRight = d;
+    }
+
+    public void setLeft(DequeNode d) {
+        this.toTheLeft = d;
+    }
 }
