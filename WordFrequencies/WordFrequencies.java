896311e33dc3d158e5b014e1263e252cb68a6488
j-woodlee <jwoodlee@lion.lmu.edu>
2015-12-10 13:18:21 -0800

    Create WordFrequencies.java

---
+++ WordFrequencies/WordFrequencies.java
@@ -0,0 +1,69 @@
+
+
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Collections;
+import java.util.ArrayList;
+
+public class WordFrequencies {
+    static HashMap<String, Integer> words = new HashMap<>();//String, occurences
+    static boolean caseSensitive;
+    static boolean clean;
+
+    public static void main(String[] args) throws Exception {
+    
+        if(args.length > 0) {
+            if(args[0].equals("-sc") || args[0].equals("-cs")) {
+                caseSensitive = true;
+                clean = true;
+            } else if(args[0].equals("-c")) {
+                clean = true;
+            } else if(args[0].equals("-s")) {
+                caseSensitive = true;
+            } else {
+                System.out.println("Usage: java WordFrequencies [-cs] < file");
+            }
+            
+        } else if(args.length > 1) {
+            System.out.println("Usage: java WordFrequencies [-cs] < file");
+        }
+
+        Scanner scan = new Scanner(System.in).useDelimiter("[^0-9a-zA-z\\-]");
+
+        while (scan.hasNext()) { // while there is a next argument
+            String s = scan.next();
+
+            s = s.replace("-", "");
+
+            if(s.length() == 0) {
+                continue;
+            }
+
+            if(!caseSensitive) {
+                s = s.toUpperCase();
+            }
+
+            if(words.get(s) == null) {
+                words.put(s,1);
+            } else {
+                words.put(s, words.get(s) + 1);
+            }
+        }
+
+        ArrayList<String> list = new ArrayList<String>(words.keySet());
+
+        Collections.sort(list);
+
+        for(String s : list) {
+            if(!caseSensitive) {
+                s = s.toUpperCase();
+            }
+
+            if(clean) {
+                System.out.println(s);
+                continue;
+            }
+            System.out.println(s + " " + words.get(s));
+        }
+    }
+}
